176c5d714d09d2d4ba9dc64cc19932a0c2de633e
Atul Dwivedi <atultechd@gmail.com>
2022-06-15 13:31:57 +0530

    34th question on linked list

---
+++ middle_of_linked_list.cpp
@@ -0,0 +1,37 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+       class Node 
+        { 
+        public:
+            int data;
+            Node *next;
+            Node(int data) 
+            {
+               this->data = data;
+              this->next = NULL;
+            }
+        };
+
+*****************************************************************/
+
+Node *findMiddle(Node *head) {
+    if(head == NULL || head->next == NULL) return head;
+    Node *slow = head;
+    Node *fast = head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+
+
+
+
+
+
+
+

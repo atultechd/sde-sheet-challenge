b96f2b1639313f3de0d0f5a70322a7dc2e8571d2
Atul Dwivedi <atultechd@gmail.com>
2022-06-18 18:04:28 +0530

    47th solution array, two pointers, sorting

---
+++ 3sum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+vector<vector<int>> findTriplets(vector<int>arr, int n, int K) {
+    sort(arr.begin(), arr.end());
+    vector<vector<int>> res;
+    for(int i=0; i<n-2; i++){
+        if(i==0 || (i>0 &&  arr[i] != arr[i-1])){
+            int low = i+1, high = arr.size()-1, sum = K-arr[i];
+            while(low<high){
+                if(arr[low] + arr[high] == sum){
+                    vector<int>temp;
+                    
+                    temp.push_back(arr[i]);
+                    temp.push_back(arr[low]);
+                    temp.push_back(arr[high]);
+                    res.push_back(temp);
+                    
+                    while(low<high && arr[low] == arr[low+1]) low++;
+                    while(low<high && arr[high] == arr[high-1]) high--;
+                    low++, high--;
+                }
+                else if(arr[low] + arr[high] < sum) low++;
+                else high--;
+            } 
+        }
+    }
+    return res;
+}

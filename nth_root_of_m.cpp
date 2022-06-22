0bc44ba4dc67407db9e9427ac2bbc8c0bc3c9815
Atul Dwivedi <atultechd@gmail.com>
2022-06-22 18:14:02 +0530

    63rd solution on divide and conquor

--- nth_root_of_m.cpp
+++ nth_root_of_m.cpp
@@ -1,1 +1,27 @@
-nth_root_of_m.cpp
+// TC: O(nLog2(m * 10^8)) SC: O(1);
+long double multiply(long double num, int n){
+    long double ans = 1.0;
+    for(int i=1; i<=n; i++){
+        ans *= num;
+    }
+    return ans;
+}
+
+long double findNthRootOfM(int n, long long m) {
+    long double low =1.0;
+    long double high = m;
+    long double esp = 1e-9;
+    while((high-low) > esp){
+        long double mid = (low+high)/2.0;
+        if(multiply(mid,n) > m) high = mid;
+        else low = mid;
+    }
+    return high;
+}
+
+// ....................................................
+
+// second solution using inbuild function pow()
+long double findNthRootOfM(int n, long long m) {
+    return pow(m, 1.000000/n);
+}

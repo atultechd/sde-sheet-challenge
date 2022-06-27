f4e944a301181f534cfbb168302f0185ee85654c
Atul Dwivedi <atultechd@gmail.com>
2022-06-28 00:47:03 +0530

    84th solution on stack using recursion

---
+++ sort_a_stack.cpp
@@ -0,0 +1,21 @@
+
+void sort_stack(stack<int> &s, int num){
+    if(s.empty() || (!s.empty() && s.top() < num)){
+        s.push(num);
+        return;
+    }
+    int n=s.top();
+    s.pop();
+    sort_stack(s,num);
+    s.push(n);
+}
+
+void sortStack(stack<int> &s)
+{
+    if(s.empty()) return;
+    int num = s.top();
+    s.pop();
+    
+    sortStack(s);
+    sort_stack(s,num);
+}

19580002360feb63b8b3736f2a5bc4126ec8e3c7
Atul Dwivedi <atultechd@gmail.com>
2022-06-30 22:20:51 +0530

    94th solution on stack

---
+++ next_smaller_element.cpp
@@ -0,0 +1,19 @@
+
+#include<bits/stdc++.h>
+vector<int> nextSmallerElement(vector<int> &arr, int n){
+    stack<int>stk;
+    vector<int>res;
+    for(int i=arr.size()-1; i>=0; i--){
+        if(stk.empty()) res.push_back(-1);
+        else{
+            while(!stk.empty() && stk.top()>=arr[i]){
+                stk.pop();
+            }
+            if(stk.empty()) res.push_back(-1);
+            else res.push_back(stk.top());
+        }
+        stk.push(arr[i]);
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}

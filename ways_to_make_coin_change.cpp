34ea0f00a37a872686ef3225704086bf967a01f9
Atul Dwivedi <atultechd@gmail.com>
2022-07-24 00:08:10 +0530

    173rd solution on dp

---
+++ ways_to_make_coin_change.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+long countWaysToMakeChange(int *denominations, int n, int value){
+
+    vector<long> dp(value+1, 0);
+
+    dp[0] = 1;
+
+    for(int i=0; i<n; i++){
+
+        for(int j=denominations[i]; j<dp.size(); j++){
+
+            dp[j] += dp[j-denominations[i]];
+        }
+    }
+    
+    return dp[value];
+}
